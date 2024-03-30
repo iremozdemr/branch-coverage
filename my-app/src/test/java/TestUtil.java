cea287d815ad055fe5536fa3a60a13745d0efdc1
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-03-30 15:08:39 +0300

    added TestUtil.java methods

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -9,8 +9,32 @@ public class TestUtil {
   Util c;
 
   @Before
-  public void setUp() { c = new Util(); }
+  public void setUp(){ 
+    c = new Util(); 
+  }
 
   @Test
-  public void example() { assertTrue(true); }
+  public void test_OneVariable(){
+    assertFalse(c.compute(3));
+  }
+
+  @Test
+  public void test_DivideByTwo(){
+    assertFalse(c.compute(4,5,6,7));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void test_Zero(){
+    c.compute(0,1,2);
+  }
+
+  @Test
+  public void test_Normal1(){
+    assertTrue(c.compute(1,2,3));
+  }
+
+  @Test
+  public void test_Normal2(){
+    assertFalse(c.compute(7,11,11));
+  }
 }
